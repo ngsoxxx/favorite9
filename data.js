2738656a9c628e833b14596d79cae3b6823e068c
ngsoxxx <sayatch28@gmail.com>
2026-09-23 00:34:49 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,290 @@
+const IDOLS = [
+  {
+    "id": "01",
+    "group": "KickFlip",
+    "name": "Amaru",
+    "image": "images/Amaru_KickFlip.jpg"
+  },
+  {
+    "id": "02",
+    "group": "GOT7",
+    "name": "BamBam",
+    "image": "images/BamBam_GOT7.jpg"
+  },
+  {
+    "id": "03",
+    "group": "Stray Kids",
+    "name": "Bang Chan",
+    "image": "images/Bang_Chan_Stray_Kids.jpg"
+  },
+  {
+    "id": "04",
+    "group": "2PM",
+    "name": "CHANSUNG",
+    "image": "images/CHANSUNG_2PM.jpg"
+  },
+  {
+    "id": "05",
+    "group": "2AM",
+    "name": "Chang Min",
+    "image": "images/Chang_Min_2AM.jpg"
+  },
+  {
+    "id": "06",
+    "group": "Stray Kids",
+    "name": "Changbin",
+    "image": "images/Changbin_Stray_Kids.jpg"
+  },
+  {
+    "id": "07",
+    "group": "DAY6",
+    "name": "DOWOON",
+    "image": "images/DOWOON_DAY6.jpg"
+  },
+  {
+    "id": "08",
+    "group": "KickFlip",
+    "name": "Donghwa",
+    "image": "images/Donghwa_KickFlip.jpg"
+  },
+  {
+    "id": "09",
+    "group": "KickFlip",
+    "name": "Donghyeon",
+    "image": "images/Donghyeon_KickFlip.jpg"
+  },
+  {
+    "id": "10",
+    "group": "Stray Kids",
+    "name": "Felix",
+    "image": "images/Felix_Stray_Kids.jpg"
+  },
+  {
+    "id": "11",
+    "group": "Xdinary Heroes",
+    "name": "Gaon",
+    "image": "images/Gaon_Xdinary_Heroes.jpg"
+  },
+  {
+    "id": "12",
+    "group": "Stray Kids",
+    "name": "HAN",
+    "image": "images/HAN_Stray_Kids.jpg"
+  },
+  {
+    "id": "13",
+    "group": "NEXZ",
+    "name": "HARU",
+    "image": "images/HARU_NEXZ.jpg"
+  },
+  {
+    "id": "14",
+    "group": "NEXZ",
+    "name": "HYUI",
+    "image": "images/HYUI_NEXZ.jpg"
+  },
+  {
+    "id": "15",
+    "group": "Stray Kids",
+    "name": "Hyunjin",
+    "image": "images/Hyunjin_Stray_Kids.jpg"
+  },
+  {
+    "id": "16",
+    "group": "Stray Kids",
+    "name": "I.N",
+    "image": "images/I.N_Stray_Kids.jpg"
+  },
+  {
+    "id": "17",
+    "group": "GOT7",
+    "name": "JAY B",
+    "image": "images/JAY_B_GOT7.jpg"
+  },
+  {
+    "id": "18",
+    "group": "2PM",
+    "name": "JUN.K",
+    "image": "images/JUN.K_2PM.png"
+  },
+  {
+    "id": "19",
+    "group": "2PM",
+    "name": "JUNHO",
+    "image": "images/JUNHO_2PM.jpg"
+  },
+  {
+    "id": "20",
+    "group": "GOT7",
+    "name": "Jackson",
+    "image": "images/Jackson_GOT7.jpg"
+  },
+  {
+    "id": "21",
+    "group": "2AM",
+    "name": "Jin Woon",
+    "image": "images/Jin_Woon_2AM.jpg"
+  },
+  {
+    "id": "22",
+    "group": "2AM",
+    "name": "Jo Kwon",
+    "image": "images/Jo_Kwon_2AM.jpg"
+  },
+  {
+    "id": "23",
+    "group": "Xdinary Heroes",
+    "name": "Jooyeon",
+    "image": "images/Jooyeon_Xdinary_Heroes.jpg"
+  },
+  {
+    "id": "24",
+    "group": "Xdinary Heroes",
+    "name": "Jun Han",
+    "image": "images/Jun_Han_Xdinary_Heroes.jpg"
+  },
+  {
+    "id": "25",
+    "group": "Xdinary Heroes",
+    "name": "Jungsu",
+    "image": "images/Jungsu_Xdinary_Heroes.jpg"
+  },
+  {
+    "id": "26",
+    "group": "GOT7",
+    "name": "Junyoung",
+    "image": "images/Junyoung_GOT7.jpg"
+  },
+  {
+    "id": "27",
+    "group": "KickFlip",
+    "name": "Juwang",
+    "image": "images/Juwang_KickFlip.jpg"
+  },
+  {
+    "id": "28",
+    "group": "KickFlip",
+    "name": "Keiju",
+    "image": "images/Keiju_KickFlip.jpg"
+  },
+  {
+    "id": "29",
+    "group": "KickFlip",
+    "name": "Kyehoon",
+    "image": "images/Kyehoon_KickFlip.jpg"
+  },
+  {
+    "id": "30",
+    "group": "Stray Kids",
+    "name": "Lee Know",
+    "image": "images/Lee_Know_Stray_Kids.jpg"
+  },
+  {
+    "id": "31",
+    "group": "GOT7",
+    "name": "Mark",
+    "image": "images/Mark_GOT7.jpg"
+  },
+  {
+    "id": "32",
+    "group": "KickFlip",
+    "name": "Minje",
+    "image": "images/Minje_KickFlip.jpg"
+  },
+  {
+    "id": "33",
+    "group": "2PM",
+    "name": "NICHKHUN",
+    "image": "images/NICHKHUN_2PM.png"
+  },
+  {
+    "id": "34",
+    "group": "Xdinary Heroes",
+    "name": "O.de",
+    "image": "images/O.de_Xdinary_Heroes.jpg"
+  },
+  {
+    "id": "35",
+    "group": "NEXZ",
+    "name": "SEITA",
+    "image": "images/SEITA_NEXZ.jpg"
+  },
+  {
+    "id": "36",
+    "group": "NEXZ",
+    "name": "SO GEON",
+    "image": "images/SO_GEON_NEXZ.jpg"
+  },
+  {
+    "id": "37",
+    "group": "DAY6",
+    "name": "SUNGJIN",
+    "image": "images/SUNGJIN_DAY6.jpg"
+  },
+  {
+    "id": "38",
+    "group": "2AM",
+    "name": "Seul Ong",
+    "image": "images/Seul_Ong_2AM.jpg"
+  },
+  {
+    "id": "39",
+    "group": "Stray Kids",
+    "name": "Seungmin",
+    "image": "images/Seungmin_Stray_Kids.jpg"
+  },
+  {
+    "id": "40",
+    "group": "2PM",
+    "name": "TAECYEON",
+    "image": "images/TAECYEON_2PM.png"
+  },
+  {
+    "id": "41",
+    "group": "NEXZ",
+    "name": "TOMOYA",
+    "image": "images/TOMOYA_NEXZ.jpg"
+  },
+  {
+    "id": "42",
+    "group": "DAY6",
+    "name": "WONPIL",
+    "image": "images/WONPIL_DAY6.jpg"
+  },
+  {
+    "id": "43",
+    "group": "2PM",
+    "name": "WOOYOUNG",
+    "image": "images/WOOYOUNG_2PM.png"
+  },
+  {
+    "id": "44",
+    "group": "NEXZ",
+    "name": "YUKI",
+    "image": "images/YUKI_NEXZ.jpg"
+  },
+  {
+    "id": "45",
+    "group": "NEXZ",
+    "name": "YU",
+    "image": "images/YU_NEXZ.jpg"
+  },
+  {
+    "id": "46",
+    "group": "DAY6",
+    "name": "Young K",
+    "image": "images/Young_K_DAY6.jpg"
+  },
+  {
+    "id": "47",
+    "group": "GOT7",
+    "name": "Youngjae",
+    "image": "images/Youngjae_GOT7.jpg"
+  },
+  {
+    "id": "48",
+    "group": "GOT7",
+    "name": "Yugyeom",
+    "image": "images/Yugyeom_GOT7.jpg"
+  }
+];
